2157b8dcb8ff3a0a384481380276a04b63b9231c
rk7892 <ranjithraina7952@gmail.com>
2023-12-26 13:03:32 +0530

    first commit

---
+++ random_colour.js
@@ -0,0 +1,67 @@
+// Random Color
+
+function generateColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+const color1 = generateColor();
+const color2 = generateColor();
+console.log(color1);
+console.log(color2);
+
+
+
+// Nested array
+
+function method2(a) {
+    let result = [];
+  
+    for (let i = 0; i < a.length; i++) {
+      if (Array.isArray(a[i])) {
+        result = result.concat(method2(a[i]));
+      } else {
+        result.push(a[i]);
+      }
+    }
+  
+    return result;
+  }
+  
+  const nestedArray = [1, [2, 3], [4, [5, 6]], 7];
+  const flattenedArray = method2(nestedArray);
+  
+  console.log(flattenedArray);
+
+
+// Remove Duplicates
+
+function method3(b) {
+    return [...new Set(b)];
+  }
+  
+  const originalArray = [1, 2, 3, 4, 5, 2, 2, 3];
+  const withoutDuplicates = method3(originalArray);
+  
+  console.log(withoutDuplicates);
+
+
+//   Count occurrences 
+
+
+var arr = [5, 5, 5, 2, 2, 2, 2, 2, 9, 4];
+var counts = {};
+for (var i = 0; i < arr.length; i++) {
+  var num = arr[i];
+
+  if (counts[num] === undefined) {
+    counts[num] = 1;
+  } else {
+    counts[num]++;
+  }
+}
+
+console.log(counts);
